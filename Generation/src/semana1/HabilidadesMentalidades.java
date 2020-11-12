e5694e20490820c4ce7e18bc2917114d16a230bc
GustavoBroch <gustavobroch@gmail.com>
2020-11-12 15:57:43 -0300

    Habilidade e Mentalidade

---
+++ Generation/src/semana1/HabilidadesMentalidades.java
@@ -0,0 +1,13 @@
+package semana1;
+
+public class HabilidadesMentalidades {
+
+	public static void main(String[] args) {
+		System.out.println("Durante essa nova trajetória dentro da Generation,"
+				+ "\ntenho buscado desenvolver o trabalho de equipe,"
+				+ "\ndurante os momenteos de dificuldade, tenho trabalhado"
+				+ "\na persistência e sempre com olhar e orientação para o futuro.  ");
+
+	}
+
+}
