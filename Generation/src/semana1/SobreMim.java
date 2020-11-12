6924503c3939cf0cc24db318840ababb8ecfbffd
GustavoBroch <gustavobroch@gmail.com>
2020-11-12 15:36:18 -0300

    Sobre mim

---
+++ Generation/src/semana1/SobreMim.java
@@ -0,0 +1,15 @@
+package semana1;
+
+public class SobreMim {
+	public static void main(String[] args) {
+		System.out.println("Meu nome é Gustavo, tenhho 27 anos."
+				+ "\nUm dos meus hobbies é praticar atividade física,"
+				+ "\nprincipalmente caminhada e corrida em parques,"
+				+ "\ntambém curto malhar nos finais de semana."
+				+ "\nNas horas vagas, gosto de sair com minha família ou amigos,"
+				+ "\npara conhecer lugares como: hamburguerias, pizzarias, etc."
+				+ "\nTambém gosto de assistir filmes e séries,"
+				+ "\nas vezes me arrisco na cozinha."
+				+ "\nNão gosto de assistir telejornais");
+	}
+}
